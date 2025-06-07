b8742bf3a66b0e77e0e1a79905fb780e96b2f779
janilcort <janilcort@gmail.com>
2025-06-07 15:28:29 -0700

    basic javascript

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,27 @@
+// This shows a popup alert with the message "Hello, World!"
+alert("Hello, World!");
+
+// Declaring a variable 'name' and assigning it the value "john"
+let name = "john";
+
+// Writing a welcome message to the web page using the 'name' variable
+document.write("Hello, " + name + "! Welcome to the JavaScript project.");
+
+// Creating a string by joining two parts and storing it in variable B
+var B = "my name" + " is john!";
+
+// Writing the value of variable B to the web page, with a line break before it
+document.write("<br>" + B);
+
+// Declaring multiple variables in one line
+// 'C' is a family name, and the rest are family members
+var C = "The Cortez", Dad = "John", Mom = "Maria", Son = "Sebby", Daughter = "Masha";
+
+// Writing the value of the Mom variable (which is "Maria") to the web page
+document.write(Mom);
+
+// This line calculates 4 + 5 but doesn't do anything with the result
+(4 + 5); // This line has no effect
+
+// This line writes the result of 4 + 5 (which is 9) to the web page
+document.write(4 + 5);
